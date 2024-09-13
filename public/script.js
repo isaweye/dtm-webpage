f1f4c638bffa6343e0388c4f8b517d8e86776063
isaweye <isaweye@gmail.com>
2024-09-13 13:19:35 +0200

    Add files via upload (ws)

--- public/script.js
+++ public/script.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const sections = document.querySelectorAll('.section');
 
+    sections.forEach(section => {
+        let html = section.innerHTML;
+        const regex = /\{#([0-9a-fA-F]{6}):([^}]+)\}/g;
+        html = html.replace(regex, function(match, color, text) {
+            return `<span class="highlight" style="border-color:#${color};">${text}</span>`;
+        });
+        section.innerHTML = html;
+    });
+
+
     function checkVisibility() {
         const windowHeight = window.innerHeight;
         const scrollY = window.scrollY;
